0dd6b28cc90a345bffde5941d377c9773bfa2045
kng1259 <kngondajob@gmail.com>
2025-04-27 20:09:18 +0700

    add talosscript and longhorn patch

---
+++ script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export TALOSCONFIG=$(realpath ./talosconfigs/talosconfig)
+control=($(virsh list | grep "talos" | grep "control" | awk '{print $2}' | xargs -L1 virsh domifaddr | awk '/ipv4/{print $4}' | cut -d'/' -f1))
+worker=($(virsh list | grep "talos" | grep "worker" | awk '{print $2}' | xargs -L1 virsh domifaddr | awk '/ipv4/{print $4}' | cut -d'/' -f1))
+talosctl gen config talos-libvirt https://192.168.121.3:6443 --install-disk /dev/vda --output talosconfigs --config-patch @talospatches/all.yaml --config-patch-control-plane @talospatches/controlplane.yaml --config-patch-worker @talospatches/worker.yaml --force
+talosctl config endpoint ${control[@]}
+talosctl -n ${control[0]} apply-config --insecure --file talosconfigs/controlplane.yaml
+sleep 300
+talosctl -n ${control[0]} bootstrap
+for i in "${control[@]:1}"; do
+    talosctl -n $i apply-config --insecure --file talosconfigs/controlplane.yaml
+done
+for i in "${worker[@]}"; do
+    talosctl -n $i apply-config --insecure --file talosconfigs/worker.yaml
+done
+talosctl -n ${control[0]} kubeconfig ./kubeconfig --force
